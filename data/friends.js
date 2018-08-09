caf4bb9edea16a0c8e86c0b88302d66166d54efd
barnumtj <tanner.barnum@icloud.com>
2018-08-09 13:07:16 -0500

    updatedlogic

--- data/friends.js
+++ data/friends.js
@@ -1,7 +1,7 @@
 let friends =
                [ {
                 "name":"Ahmed",
-                "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+                "photo":"/Users/kaceehiggins/Documents/BootCamp/FriendFinder/app/friend-finder/data/images/tannerfarmer.jpg",
                 "scores":[
                     5,
                     1,
@@ -16,18 +16,66 @@ let friends =
                 ]
             }, {
                 "name": "Tanner",
-                "Photo": "",
+                "Photo": "https://www.omaha.com/huskers/football/tanner-farmer-believes-he-can-lift-pounds-and-win-a/article_5f70d460-180b-5727-9dcf-28fa912cf7bd.html",
                 "scores":[
+                    5,
+                    2,
+                    2,
+                    3,
                     5,
                     1,
                     4,
+                    5,
                     4,
+                    4
+                ]
+            },
+            {
+                "name": "LeBron",
+                "Photo": "",
+                "scores":[
+                    1,
+                    3,
+                    2,
+                    1,
+                    5,
+                    5,
+                    2,
+                    5,
+                    2,
+                    1
+                ]
+            },
+            {
+                "name": "Patrick Mahomes",
+                "Photo": "",
+                "scores":[
+                    1,
+                    5,
+                    2,
+                    2,
                     5,
                     1,
                     2,
                     5,
                     4,
-                    1
+                    5
+                ]
+            },
+            {
+                "name": "Tyreek Hill",
+                "Photo": "",
+                "scores":[
+                    1,
+                    1,
+                    1,
+                    1,
+                    5,
+                    5,
+                    5,
+                    5,
+                    4,
+                    5
                 ]
             }
         ]
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -13,13 +13,54 @@ app.post('/api/friends', function(req, res){
 	// setting a variable for the user's response
     var userInfo = req.body;
     // var userParse = JSON.parse(userInfo)
-    console.log(userInfo.name)
+    // console.log(userInfo.name)
     // res.send(userInfo)
     friendslist.push(userInfo);
-    res.send(JSON.stringify(userInfo))
+   
+
+
+    let bestFriend = compareUser(userInfo)
+    // console.log(bestFriend)
+    res.json(bestFriend)
+
+
+
+
+
+
 
 // compare userinfo against friendslist, math abs
 // { name, photo, scores } = req.body
 });
 
+}
+
+function compareUser(user) {
+    let bestScore = 1000;
+    let bestFriend;
+    let friendsScore = 0
+    let userScore = 0
+    let friendsArray = []
+    for (let i = 0; i < user.scores.length; i++) {
+        userScore += parseInt(user.scores[i]);
+        // console.log(userScore)
+    } 
+    for (let j = 0; j < friendslist.length; j++) {
+        let currentFriend = friendslist[j];
+       
+      
+        for (let j = 0; j < currentFriend.scores.length; j++) {
+            
+            friendsScore += parseInt(currentFriend.scores[j])
+            
+           
+            
+        }
+        let score = Math.abs(userScore - friendsScore)
+        if (score < bestScore) {
+            bestScore = score;
+            bestFriend = currentFriend;
+            
+        }
+    } return bestFriend
 }
